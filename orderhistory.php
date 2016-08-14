c4399b4e95be1905742833472621d3fdfd495cca
Gema Aji Wardian <gema_wardian@hotmail.com>
2016-08-14 18:11:37 +0700

    complete order overhaul

--- orderhistory.php
+++ orderhistory.php
@@ -6,16 +6,41 @@
 
     $username = $_SESSION['username'];
 
-    $query = "SELECT ID FROM users WHERE username='$username'";
+    $query2 = "SELECT ID FROM users WHERE username LIKE '$username'";
+    $results = mysqli_query($connect, $query2) or die(mysql_error());
+    $id_data = mysqli_fetch_array($results, MYSQLI_ASSOC);
 
-    $query = "SELECT * FROM order_data WHERE user_id = '$userid'";
+    $iddata = $id_data['ID'];
+
+    $query = "SELECT * FROM order_data WHERE user_id = '$iddata'";
     $results = mysqli_query($connect, $query) or die (mysql_query());
     
     if(mysqli_num_rows($results)==0)
     {
-	    echo "Your Cart is empty";
+	    echo "Your Order is empty";
     } else { 
 ?>
+<h3>Order Data</h3>
+<table border="0" align="center" cellpadding="5">
+  	<tr><td> Transaction ID</td><td>Time of Order</td><td>Status</td><td>Shipping name</td><td>Total Price</td></tr>
+<?php
+	while ($row = mysqli_fetch_array($results, MYSQLI_ASSOC)) {
+  		extract($row);
+  		echo "<tr><td>";
+  		echo $id;
+  		echo "</td>";
+  		echo "<td>";
+  		echo $timestamp;
+  		echo "</td><td>";
+  		echo $status;
+  		echo "</td><td>";
+  		echo $shipping_name;
+  		echo "</td><td>";
+        echo $product_cost;
+        echo "</td></tr>";
+    }
+?>
+</table>
 <?php
     }
     include('footer.php');
